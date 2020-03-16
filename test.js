6ac34f53018bc03fedc90f839c957fe673a4cfe1
SolarFlare404 <43080450+SolarFlare404@users.noreply.github.com>
2020-03-15 21:42:39 -0700

    Added mousedown/up on class

--- test.js
+++ test.js
@@ -1,1 +1,31 @@
-console.log('H377o World!');
+window.addEventListener("load", main)
+
+
+function main()
+{
+  let elem = document.getElementById("mrb");
+  
+  //elem.addEventListener("click", onClick);
+  
+  elem.addEventListener("mousedown", onMouseDown)
+  elem.addEventListener("mouseup"  , onMouseUp)
+  
+  console.log('H377o World!');
+  console.log(elem);
+}
+
+function onMouseDown(event)
+{
+  event.currentTarget.classList.add('Task');
+}
+
+function onMouseUp(event)
+{
+  event.currentTarget.classList.remove('Task');
+}
+
+function onClick(event)
+{
+  //current target is what the listener is tied to
+  event.currentTarget.classList.add('Task');
+}
